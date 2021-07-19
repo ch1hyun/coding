7c0e23f19268fae3b059db07623678303070a2e4
chyun <72855914+ch1hyun@users.noreply.github.com>
2021-07-19 20:48:18 +0900

    Update p-1920.cpp

--- baekjoon/p-1920.cpp
+++ baekjoon/p-1920.cpp
@@ -1,59 +1,51 @@
-// 시간 초과
+// endl이 버퍼를 비워줌과 동시에 개행을 수행하기 때문에 시간 초과가 되었다.
 
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-void insertionSort(int arr[], int size) {
-	int i, j, key;
-	for(int i = 1; i < size; ++i) {
-		j = i - 1;
-		key = arr[i];
-		while(j >= 0 && arr[j] > key) {
-			arr[j + 1] = arr[j];
-			--j;
-		}
-		arr[j + 1] = key;
-	}
-}
-
-int binarySearch(int arr[], int target, int size) {
-	int mid;
-	int low = 0;
-	int high = size - 1;
+void binarySearch(int arr1[], int arr2[], int size1, int size2) {
+	int i, mid, low, high;
 	
-	while(low <= high) {
-		mid = (low + high) / 2;
+	for (i=0;i<size2;++i) {
+		low = 0;
+		high = size1 - 1;
 		
-		if (arr[mid] == target) return mid;
-		else if (arr[mid] > target) high = mid - 1;
-		else low = mid + 1;
+		while(low <= high) {
+			
+			mid = (low + high) / 2;
+
+			if (arr1[mid] == arr2[i]) {
+				cout << "1\n";
+				break;
+			}
+			else if (arr1[mid] > arr2[i]) high = mid - 1;
+			else low = mid + 1;
+		}
+		if (low > high) cout << "0\n";
 	}
-	
-	return -1;
 }
 
 int main() {
-	int num, target, result;
-	double result_c;
-	time_t start, end;
-	cin >> num;
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 	
-	int arr[num];
-	for(int i = 0; i < num; ++i) {
-		cin >> arr[i];
-	}
-	insertionSort(arr, num);
+	int i, num1, num2, temp;
+	cin >> num1;
 	
-	cin >> num;
-	int find[num];
-	for(int i = 0; i < num; ++i) {
-		cin >> find[i];
+	int arr1[num1];
+	for(i = 0; i < num1; ++i) {
+		cin >> temp;
+		arr1[i] = temp;
+	}
+	sort(arr1, arr1+num1);
+	
+	cin >> num2;
+	int arr2[num2];
+	for(i = 0; i < num2; ++i) {
+		cin >> arr2[i];
 	}
 	
-	for (int i = 0; i < num; ++i) {
-		if(binarySearch(arr, find[i], num) != -1) cout << "1" << endl;
-		else cout << "0" << endl;
-	}
+	binarySearch(arr1, arr2, num1, num2);
 	
 	return 0;
 }
